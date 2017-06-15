edfceeb17ccff57f7c0568f812e0b24e7f54564a
ja <ja@172.25.169.220>
2017-06-16 00:14:12 +0200

    waiter fill

--- Restaurant/Window.java
+++ Restaurant/Window.java
@@ -94,7 +94,7 @@ public class Window extends JPanel{
 		
 		g2.setColor(Color.RED);
 		for (Waiter w: RestaurationManager.Instance().WaiterList) {
-			g2.drawRect((int)w.getPosition().getX(), (int)w.getPosition().getY(), 25, 25);
+			g2.fillRect((int)w.getPosition().getX(), (int)w.getPosition().getY(), 25, 25);
 		} 
 	}
 } 
