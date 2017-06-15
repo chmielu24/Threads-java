21822dbb76943d44a94ccf19ace56c2594c7433e
ja <ja@172.25.169.220>
2017-06-15 21:17:19 +0200

    add frame

--- Restaurant/Window.java
+++ Restaurant/Window.java
@@ -1,22 +1,29 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class Window {
-	
+public class Window extends JPanel{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SwingUtilities.invokeLater(new Runnable(){
-
+			
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
-				
+				JFrame restaurantFrame = new JFrame("restaurant");
+				restaurantFrame.setSize(800,800);
+				restaurantFrame.setResizable(false);
+				restaurantFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				restaurantFrame.add(new Window());
+				restaurantFrame.setVisible(true);		
 			}
 		});
 
 	}
 	
 	public Window(){
-
+		
 	}
+	
+	
 } 
