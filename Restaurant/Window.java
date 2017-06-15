64a22620e153e6a86b5c00aadf7322e537c688ac
ja <ja@172.25.169.220>
2017-06-15 20:39:22 +0200

    window

--- Restaurant/Window.java
+++ Restaurant/Window.java
@@ -1,7 +1,24 @@
+import javax.swing.SwingUtilities;
 
 public class Window {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				MyFrame myframe = new MyFrame("restauracja");
+			}
+		});
+}
+	
+	
 	public Window(){
 		
-		//zmieniam
+		
+		
 	}
 } 
