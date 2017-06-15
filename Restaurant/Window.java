37398b67e4e7298793449da85f4bccf69b11de91
chmielu24 <dawids42@gmail.com>
2017-06-15 22:27:14 +0200

    added invoke Manager

--- Restaurant/Window.java
+++ Restaurant/Window.java
@@ -11,6 +11,8 @@ import javax.swing.SwingUtilities;
 
 public class Window extends JPanel{
 	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SwingUtilities.invokeLater(new Runnable(){
@@ -41,7 +43,7 @@ public class Window extends JPanel{
 			e.printStackTrace();
 		}
 		
-	
+		new RestaurationManager().start();
 	}
 	
 	@Override
